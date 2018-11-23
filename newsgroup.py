bad94ce5165affeb9b9eb6769b51bdc998b13c89
Mohamed El Halaby <41085154+el-halaby@users.noreply.github.com>
2018-11-23 03:28:47 +0200

    Update newsgroup.py

--- newsgroup.py
+++ newsgroup.py
@@ -8,13 +8,25 @@ from sklearn.svm import LinearSVC, NuSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 
-def get_subjects(filename, cls):
+def by_subject(filename, cls):
     data = open(filename)
     lines = [line for line in data]
     print('len:',len(lines))
     X = []
     for i in range(0,len(lines)):
         if lines[i].startswith('Subject:') and lines[i-1].startswith('From:'):
+            X.append(lines[i])
+    target = [cls for i in range(0, len(X))]
+    print('Count',filename,':', len(target))
+    return X, target
+
+def by_email(filename, cls):
+    data = open(filename)
+    lines = [line for line in data]
+    print('len:',len(lines))
+    X = []
+    for i in range(0,len(lines)):
+        if lines[i].startswith('From:') and lines[i+1].startswith('Subject:'):
             X.append(lines[i])
     target = [cls for i in range(0, len(X))]
     print('Count',filename,':', len(target))
@@ -29,7 +41,8 @@ if __name__ == '__main__':
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):    
             print('filename:', filename)
-            data, target = get_subjects(directory+filename, count)
+            #data, target = by_email(directory+filename, count) 
+            data, target = by_subject(directory+filename, count)
             for x, y in zip(data, target):
                 X.append(x)
                 Y.append(y)
@@ -43,14 +56,14 @@ if __name__ == '__main__':
     X_new = TfidfVectorizer(stop_words = 'english').fit_transform(X)
 
     # feature transformation
-    X_new = MaxAbsScaler().fit_transform(X_new)
+    X_new = Normalizer().fit_transform(X_new)
     print(X_new.shape)
     
     # feature selection
     X_new = SelectFromModel(LinearSVC(penalty='l1', dual=False)).fit_transform(X_new, Y)
     print(X_new.shape)
 
-    clf = LinearSVC(penalty='l1', dual=False)
+    clf = LinearSVC(penalty='l2', dual=True)
     results = cross_val_score(clf, X_new, Y, cv=10, n_jobs = 10)
     print('Mean:', results.mean()*100)
     print('STD:', results.std()*100)
